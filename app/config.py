f57e83631e2a3ee7b1480152ef268434df19fba8
Manik Kaushik <testing@testing.com>
2025-08-15 18:21:18 +0530

    Updated DB connection to use Render credentials

--- app/config.py
+++ app/config.py
@@ -6,14 +6,7 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
 
-    database_hostname: str
-    database_username: str
-    database_name: str
-    database_port: str
-    database_password: str
-    secret_key: str
-    algorithm: str
-    access_token_expire_minutes: int
+    database_url: str
 
     class Config:
         env_file = ".env"
